f6806e4573f9135bd79d4130cf0e3217d6b36da0
Slok-e <106457601+Slok-e@users.noreply.github.com>
2023-01-04 23:09:44 -0500

    models2

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return super().__str__()
 
 
 class Choice(models.Model):
